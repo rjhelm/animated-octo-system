3601665f1bd96a22946d02817951fc9086d44233
rjhelm <rjhelm@github.io>
2021-08-14 14:23:44 -0600

    created paypal component

---
+++ client/src/screens/OrderScreen.js
@@ -0,0 +1,5 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { createOrder, detailsOrder, payOrder } from '../actions/orderActions';
+import PaypalButton from '../components/PaypalButton';
